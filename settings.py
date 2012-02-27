751ba9c0c2bc218106dc0dcfcc540b544e4bd00c
Jacob Richard <jacob.richard@gmail.com>
2012-02-27 13:33:14 -0600

    Changing order of template resolution

--- settings.py
+++ settings.py
@@ -78,7 +78,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/usr/local/lib/python2.6/dist-packages/Django-1.3.1-py2.6.egg/django/contrib/admin/templates/admin',
+#    '/usr/local/lib/python2.6/dist-packages/Django-1.3.1-py2.6.egg/django/contrib/admin/templates/admin',
 	'/root/django-webapp/templates',
 )
 
